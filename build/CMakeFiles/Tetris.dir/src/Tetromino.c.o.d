b6efb0369212c7abc9d95931d45ee37402acc1b5
Skullkrash <jeannotnaegely@gmail.com>
2025-06-24 14:09:03 +0200

    feat: Ignored all build / compiled files that are irrelevant to the project

--- build/CMakeFiles/Tetris.dir/src/Tetromino.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/Tetris.dir/src/Tetromino.c.o: \
- /home/skullkrash/algorithmique/Tetris/druenne-naegely-tetris/src/Tetromino.c \
- /usr/include/stdc-predef.h \
- /home/skullkrash/algorithmique/Tetris/druenne-naegely-tetris/src/Tetromino.h
